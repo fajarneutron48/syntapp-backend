43370d3e3e3559a021e74d1a152d879f6dc0e2dd
fajarneutron48 <fajarneutron48@gmail.com>
2023-08-12 18:01:38 +0700

    re-patch database

--- app/config/database.js
+++ app/config/database.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "id21084887_root",
-  password: "Fajaradmin123",
-  database: "id21084887_synt_app_db",
+  host: "mariaDbSyntNodejs",
+  user: "root",
+  password: "fajaradmin",
+  database: "synt_db",
   port: 3306,
 });
 
